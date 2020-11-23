09274df879647e4cce7aeac4309a138e02316a03
songjialin <songjialin@50lion.com>
2020-11-23 18:17:32 +0800

    docker

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,6 +7,7 @@ docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2":/root/.m2  -w /usr
 
 IMG_NAME="xmshop-front"
 TIMESTAMP=$(date "+%Y%m%d%H%M%S")
+PORT_MAPPING="8080:8080"
 case $1 in
 f)
   cp xm_shop_front_web/target/xmShopFront.war docker/ROOT.war
@@ -14,6 +15,7 @@ f)
 b)
   cp xm_shop_back_web/target/xmShopBack.war docker/ROOT.war
   IMG_NAME="xmshop-back"
+  PORT_MAPPING="8081:8081"
   ;;
 *)
   echo '参数不合法! 请指定需要打包的系统 b=后台系统 f=商城系统'
@@ -22,3 +24,5 @@ b)
 esac
 docker build -t "$IMG_NAME":"$TIMESTAMP" docker
 docker tag "$IMG_NAME":"$TIMESTAMP" "$IMG_NAME":latest
+docker stop $IMG_NAME || echo 关闭$IMG_NAME失败,容器不存在或无法关闭,请留意
+docker run -d --rm --name=$IMG_NAME -p $PORT_MAPPING "$IMG_NAME":latest
